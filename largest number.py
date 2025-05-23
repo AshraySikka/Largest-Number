c27d7e7160bb75f6b8fe8d1fecd595316ab5f042
Ashray Sikka <69213851+AshraySikka@users.noreply.github.com>
2025-05-22 21:08:52 -0400

    Create largest number.py

---
+++ largest number.py
@@ -0,0 +1,11 @@
+i=int(input('Enter the number of numbers you want to find the largest of: '))
+l=int(input("Enter number: "))
+i-=1
+while i!=0:
+    j=int(input("Enter number: "))
+    if j>l:
+        l=j
+    i-=1
+print("Largest number of the group: ", l)
+s=5//2
+print(s)
